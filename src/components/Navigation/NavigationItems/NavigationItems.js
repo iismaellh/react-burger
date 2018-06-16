fba6a728904e16cee546820579640aca8fd97675
iismaell <sighlence13@hotmail.com>
2018-06-16 17:19:43 +0800

    firebase data security

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,11 +3,17 @@ import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
+const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" exact>Burger Builder</NavigationItem>
-        <NavigationItem link="/orders">Orders</NavigationItem>
-        <NavigationItem link="/auth">Auth</NavigationItem>
+        <NavigationItem link="/" exact>Builder</NavigationItem>
+        
+        {props.isAuthenticated 
+            ? <NavigationItem link="/orders">Orders</NavigationItem>
+            : null }
+
+        {props.isAuthenticated 
+            ? <NavigationItem link="/logout">Logout</NavigationItem>
+            : <NavigationItem link="/auth">Auth</NavigationItem> }
     </ul>
 );
 
